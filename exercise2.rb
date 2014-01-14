c26d8f99ba3868fa393c892817d8bb727bf4bc2f
Luke Walker <luke.t.walker@gmail.com>
2014-01-14 12:03:59 -0500

    pushing completed exercise 2

--- exercise2.rb
+++ exercise2.rb
@@ -2,3 +2,14 @@
 puts 55* 0.20
 
 
+puts "hi" + 4.to_s
+
+a = 45628
+b = 7839
+
+# string interpolatin' my way to the top
+puts "if we multipy #{a} with #{b} we get #{a*b}"
+
+
+#boolean
+puts (true && false) || (false && true) || !(false && false)
